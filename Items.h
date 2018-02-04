88d168a29da4991b37f5ed10b1fdfc54e2a30348
Valentin Pearce <valentin.pearce@forgetfulmuffin.fr>
2018-02-04 15:27:38 +0100

    Added include guards for items and monsters

--- Items.h
+++ Items.h
@@ -1,5 +1,20 @@
+#ifndef ITEMS_H
+
+#define ITEMS_H
+
 struct _item {
-	str name, list ;
+	str name, listName ;
 	int type, modifier};
 
 typedef struct _item item;
+
+struct _inventory {
+	struct _inventory * previous;
+	struct _inventory * next;
+	item data;
+}
+
+typedef struct _inventory * inventory;
+
+
+#endif /* end of include guard: ITEMS_H */
--- Monsters.h
+++ Monsters.h
@@ -1,6 +1,13 @@
+#ifndef MONSTERS_H
+
+#define MONSTERS_H
+
 struct _monster {
 	str name ;
 	int id, rate, power, health, xp ;
 	int[] levels};
 
 typedef struct _monster monster;
+
+
+#endif /* end of include guard: MONSTERS_H */
